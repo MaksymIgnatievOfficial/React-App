0ff8d8535164a5a1a562a996b23543e9ba578ae2
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-01-31 16:49:08 +0100

    test3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,13 @@ import "../assets/App/css/App.css"
 import Footer from "./blocks/_footer"
 import TodoBlock from "./blocks/_todoBlock"
 import LabelCoppied from "./blocks/_text_copied_label"
-export default function App(props) {
+import { useState, useRef } from "react"
+export default function App() {
+	const [classes, setClasses] = useState(["coppied-label"])
+
 	return (
 		<div className="App">
-			<LabelCoppied />
+			<LabelCoppied classes={classes} />
 			<TodoBlock />
 			<Footer />
 		</div>
